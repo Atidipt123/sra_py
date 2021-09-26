6a6601eb75eac37072db9859ff433e773cea1ce8
Atidipt123 <70474889+Atidipt123@users.noreply.github.com>
2021-09-26 11:53:32 +0530

    Create dog.py

---
+++ examples/dog.py
@@ -0,0 +1,17 @@
+"""
+Example
+
+Dog Image
+"""
+import sra_py
+
+
+# There are two ways to do this
+
+dog_img = sra_py.dog()
+print(dog_img)
+
+# or
+
+second_dog_image = sra_py.image("dog")
+print(second_dog_image)
